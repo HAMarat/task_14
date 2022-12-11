f9d8dd11912284b17d1af84b8c965901109b7a4d
Huawei_station <smile56680@gmail.com>
2022-12-11 21:03:31 +0300

    3

--- utils.py
+++ utils.py
@@ -2,15 +2,17 @@ import sqlite3 as sql
 
 
 def get_by_title(query_title):
+    """
+    Функция поиска фильмов в базе данных по названию
+    """
     with sql.connect('netflix.db') as con:
         con.row_factory = sql.Row
         cur = con.cursor()
-        query = f"""
-        SELECT title, country, release_year, listed_in, description
-        FROM netflix
-        WHERE title LIKE '%{query_title}%'
-        ORDER BY date_added DESC
-        """
+        query = (f"SELECT title, country, release_year, listed_in, description\n"
+                 f"FROM netflix\n"
+                 f"WHERE title LIKE '%{query_title}%'\n"
+                 f"ORDER BY date_added DESC\n"
+                 )
         cur.execute(query)
         query_result = cur.fetchone()
         result = None
@@ -26,6 +28,9 @@ def get_by_title(query_title):
 
 
 def get_by_years(year_1, year_2):
+    """
+    Функция поиска фильмов в базе данных по годам выхода
+    """
     with sql.connect('netflix.db') as con:
         result = []
         con.row_factory = sql.Row
@@ -48,6 +53,9 @@ def get_by_years(year_1, year_2):
 
 
 def get_by_rating(rating_movie):
+    """
+    Функция поиска фильмов в базе данных по рейтингу
+    """
     with sql.connect('netflix.db') as con:
         result = []
         con.row_factory = sql.Row
@@ -71,6 +79,9 @@ def get_by_rating(rating_movie):
 
 
 def get_by_genre(genre_movie):
+    """
+    Функция поиска фильмов в базе данных по жанру
+    """
     with sql.connect('netflix.db') as con:
         result = []
         con.row_factory = sql.Row
@@ -93,17 +104,18 @@ def get_by_genre(genre_movie):
         return result
 
 
-def get_by_type(type, release_year, listed_in):
+def get_by_type(type_movie, release_year_movie, listed_in_movie):
+    """
+    Функция поиска фильмов в базе данных по данным типа фильма, года выпуска и жанра
+    """
     with sql.connect('netflix.db') as con:
         result = []
         con.row_factory = sql.Row
         cur = con.cursor()
         query = f"""
-        SELECT title, release_year, listed_in
+        SELECT title, description
         FROM netflix
-        WHERE listed_in LIKE '%{genre_movie}%'
-        ORDER BY release_year DESC
-        LIMIT 10
+        WHERE type = {type_movie} AND release_year = {release_year_movie} AND listed_in LIKE '%{listed_in_movie}%'
         """
         cur.execute(query)
         query_result = cur.fetchall()
