ebaee3c14b797b096a29c40290953391ba51209f
Huawei_station <smile56680@gmail.com>
2022-12-12 18:02:09 +0300

    5

--- utils.py
+++ utils.py
@@ -8,14 +8,13 @@ def get_by_title(query_title):
     with sql.connect('netflix.db') as con:
         con.row_factory = sql.Row
         cur = con.cursor()
-        query = (f"SELECT title, country, release_year, listed_in, description\n"
-                 f"FROM netflix\n"
-                 f"WHERE title LIKE '%{query_title}%'\n"
-                 f"ORDER BY date_added DESC\n"
+        query = (f'SELECT title, country, release_year, listed_in, description\n'
+                 f'FROM "netflix"\n'
+                 f'WHERE title LIKE \'%{query_title}%\'\n'
+                 f'ORDER BY date_added DESC\n'
                  )
         cur.execute(query)
         query_result = cur.fetchone()
-        result = None
         if query_result:
             result = {
                     "title": query_result["title"],
@@ -104,7 +103,35 @@ def get_by_genre(genre_movie):
         return result
 
 
-def get_by_type(type_movie, release_year_movie, listed_in_movie):
+def get_actors(actor_1, actor_2):
+    """
+    Функция поиска актеров, которые играли с данными актерами больше двух раз
+    """
+    with sql.connect('netflix.db') as con:
+        result = []
+        cur = con.cursor()
+        query = f"""
+        SELECT netflix.cast
+        FROM netflix
+        WHERE netflix.cast LIKE '%{actor_1}%' AND netflix.cast LIKE '%{actor_2}%'
+        """
+        cur.execute(query)
+        query_result = cur.fetchall()
+        result = {}
+        for data in query_result:
+            for actors in list(data):
+                for actor in actors.split(', '):
+                    if actor not in (actor_1, actor_2):
+                        result[actor] = result.get(actor, 0) + 1
+        actors_list = []
+        for key, values in result.items():
+            if values > 2:
+                actors_list.append(key)
+
+        return actors_list
+
+
+def get_by_type_year_genre(type_movie, release_year_movie, listed_in_movie):
     """
     Функция поиска фильмов в базе данных по данным типа фильма, года выпуска и жанра
     """
@@ -115,7 +142,9 @@ def get_by_type(type_movie, release_year_movie, listed_in_movie):
         query = f"""
         SELECT title, description
         FROM netflix
-        WHERE type = {type_movie} AND release_year = {release_year_movie} AND listed_in LIKE '%{listed_in_movie}%'
+        WHERE "type" = '{type_movie}' 
+        AND netflix.release_year = {release_year_movie} 
+        AND listed_in LIKE '%{listed_in_movie}%'
         """
         cur.execute(query)
         query_result = cur.fetchall()
